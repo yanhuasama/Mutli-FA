bcc27ebd85cf19548705fd9d7e87fe18dc57fcb0
yanhuasama <50227062+yanhuasama@users.noreply.github.com>
2024-04-18 16:33:45 +0800

    Delete Mutli-FA directory

--- Mutli-FA/datasets/__init__.py
+++
@@ -1,8 +0,0 @@
-from .datasets import make
-from . import mini_imagenet
-from . import tiered_imagenet
-from . import cifar_fs
-from . import fc100
-from . import image_folder
-from . import samplers
-
